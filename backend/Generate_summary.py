eab948f7e14ac1135504df2642cca4617d69dd9c
harsh patel <hp333854@gmail.com>
2025-08-14 18:14:25 +0530

    final test

--- backend/Generate_summary.py
+++ backend/Generate_summary.py
@@ -4,16 +4,13 @@ from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 class Generate_summary():
     def __init__(self):
         model = "google/long-t5-tglobal-base"
-        if torch.cuda.is_available():
-            self.device = torch.device("cuda")
-        else:
-            self.device = torch.device("cpu")
+        self.device = torch.device("cpu")
         self.tokenizer = AutoTokenizer.from_pretrained(model)
         self.model = AutoModelForSeq2SeqLM.from_pretrained(model).to(self.device)
 
     def summury_generated(self, text):
         text = "summarize: " + text
-        inputs = self.tokenizer(text, return_tensors="pt", max_length=4096, truncation=True).to("cuda")
+        inputs = self.tokenizer(text, return_tensors="pt", max_length=4096, truncation=True).to(self.device)
 
         summary_ids = self.model.generate(inputs["input_ids"], max_length=256, num_beams=4, early_stopping=True)
         summary = self.tokenizer.decode(summary_ids[0], skip_special_tokens=True)
--- frontend/frontend.py
+++ frontend/frontend.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import requests
 import hashlib
+import whisper
+from pydub import AudioSegment
+import tempfile
 
 API = "http://127.0.0.1:8000"
 
@@ -10,7 +13,7 @@ with center:
     st.header("Text Summurizer")
 
 st.title("📂 Choose Input Type")
-input_type = st.radio("Select input type:", ["PDF", "Video"])
+input_type = st.radio("Select input type:", ["PDF", "Audio"])
 
 if input_type == "PDF":
     file = st.file_uploader(
@@ -51,19 +54,48 @@ if input_type == "PDF":
             else:
                 st.error("Failed to generate text.")
 else:
-    video = st.file_uploader(
-        "Drag and drop a video here or click to browse",
-        type = [".mp4", ".jpeg", ".png"],
+    Audio = st.file_uploader(
+        "Drag and drop a Audio here or click to browse",
+        type = [".wav",".mp3",".m4a",".flac",".ogg",".opus",".aac",".wma"],
         help = "Only Video or Image files allowed",
         label_visibility = "collapsed"
     )
-    if video:
-        st.success(f"PDF Uploaded: {video.name}")
-        
-        # Placeholder for actual PDF processing
-        # Replace with your own logic
-        summary_text = "This is a dummy summary of the uploaded PDF."
-        
-        # Display summary
-        st.subheader("📝 Generated Summary:")
-        st.text_area("Summary Output", summary_text, height=200)
+    if Audio:
+        model = whisper.load_model("tiny")
+        if Audio is not None:
+            sound = AudioSegment.from_file(Audio)
+            raw_data = sound.raw_data
+            file_hash = hashlib.md5(raw_data).hexdigest()
+
+        if ("file_hash" not in st.session_state) or (st.session_state["file_hash"] != file_hash):
+            # Save to a temporary file in a format Whisper likes
+            with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as tmp_file:
+                temp_path = tmp_file.name
+                sound.set_frame_rate(16000).set_channels(1).export(temp_path, format="wav")
+
+            # Now transcribe safely
+            result = model.transcribe(temp_path)
+            text = result["text"]
+            res = requests.post(f"{API}/upload", json = {
+                "file_name" : Audio.name,
+                "file_content" : text
+            })
+
+            if res.status_code == 200:
+                st.session_state["file_id"] = res.json()["file_id"]
+                st.session_state["file_hash"] = file_hash
+                st.success("file Uploaded.")
+            else:
+                st.error("Couldn't handle file please reupload it!")
+
+        if st.button("Generate summary") and "file_id" in st.session_state:
+            res = requests.post(f"{API}/Generatetext", json = {
+                "file_id" : st.session_state["file_id"]
+            })
+
+            if res.status_code == 200:
+                summary = res.json().get("summary", "")
+                st.subheader("📝 Generated Summary:")
+                st.text_area("Summary Output", summary, height=200)
+            else:
+                st.error("Failed to generate text.")
